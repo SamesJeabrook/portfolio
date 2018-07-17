5562babde14a0e56065262c1f895c648ae474d31
MrSlips <james.seabrook@brandworkz.com>
2018-07-17 12:32:14 +0100

    added an adjustment for getting the react app to start

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ app.use(function(err, req, res, next) {
 
 if(process.env.NODE_ENV === 'production'){
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'site/public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'site/src', 'index.js'));
   });
 }
 
